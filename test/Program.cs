8060bf4d711a7f9421b2dff101c588dac0b1ce1d
JeanCarlosVal <Jean_Valenzuela2021@outlook.com>
2024-12-21 18:47:13 -0500

    Added dummy project to test functionality :test_tube:

---
+++ test/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace test;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string hello = "Hello World!!";
+
+        foreach (var c in hello)
+        {
+            //Converitng each letter to its integer value
+            Console.WriteLine((int)c);
+        }
+
+        int letter = 32;
+
+        Console.WriteLine((char)letter);
+        
+    }
+}
