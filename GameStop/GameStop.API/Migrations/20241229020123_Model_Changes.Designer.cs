75abfbb112a7f58fb56166eaa89f3b8ed44c27df
Jean Valenzuela <Jean_Valenzuela2021@outlook.com>
2024-12-28 21:03:43 -0500

    modified review model migration

--- GameStop/GameStop.API/Migrations/20241229020123_Model_Changes.Designer.cs
+++ GameStop/GameStop.API/Migrations/20241229020123_Model_Changes.Designer.cs
@@ -4,6 +4,7 @@ using GameStop.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GameStop.API.Migrations
 {
     [DbContext(typeof(GameStopContext))]
-    partial class GameStopContextModelSnapshot : ModelSnapshot
+    [Migration("20241229020123_Model_Changes")]
+    partial class Model_Changes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
